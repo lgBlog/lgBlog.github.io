fba3a66d2125934b1db0f60356529020bb496d39
sktoo0 <84497351+lgBlog@users.noreply.github.com>
2021-05-29 08:50:15 +0800

    e

---
+++ googleAnlytics.js
@@ -0,0 +1,10 @@
+
+
+(function ($) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-JKYVBTNY71');
+})
+
+
